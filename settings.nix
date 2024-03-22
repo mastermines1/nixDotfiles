b092c482616d1d82d477e3354e8f60be325642f6
Mastermines1 <85805049+mastermines1@users.noreply.github.com>
2024-03-22 23:58:38 +0000

    3 current   2024-03-22 23:16:45  23.11.20240320.f091af0   6.1.82                          *

--- settings.nix
+++ settings.nix
@@ -12,8 +12,8 @@
     email = "milespatrickcampbell@gmail.com"; # Your email (used for git).
     gitName = "mastermines1";
     theme = "everforest"; # The name of the theme in the ./themes folder.
-    wallpaper = "/home/milesc/.dotfiles/themes/everforest/wallpaper.jpg"; #Set as "" to use wallpaper in theme file.
-    reThemeWall = false; # Re-theme the colours of the paper with imagemagic
+    wallpaper = "/home/milesc/.dotfiles/themes/everforest/wallpaper.png"; #Set as "" to use wallpaper in theme file.
+    reThemeWall = true; # Re-theme the colours of the paper with imagemagic
     wm = "hyprland";
     font = "Iosevka Aile";
     fontPkg = "iosevka";
