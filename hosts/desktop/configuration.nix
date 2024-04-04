0379788f0d269028c8f52a524b4bbb15bcbbed4c
mastermines1 <milespatrickcampbell@gmail.com>
2024-04-04 16:51:02 +0100

    idk

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -22,12 +22,12 @@
  fileSystems."/run/mount/HHD" = {
    device = "/dev/disk/by-uuid/68932330-21aa-4e00-81c2-1bd38218bf34";
    fsType = "ext4";
-   options = [ # If you don't have this options attribute, it'll default to "defaults" 
+#   options = [ # If you don't have this options attribute, it'll default to "defaults" 
      # boot options for fstab. Search up fstab mount options you can use
-     "users" # Allows any user to mount and unmount
-     "nofail" # Prevent system from failing if this drive doesn't mount
+#     "users" # Allows any user to mount and unmount
+#     "nofail" # Prevent system from failing if this drive doesn't mount
      
-   ];
+#   ];
  };
 
   networking = {
