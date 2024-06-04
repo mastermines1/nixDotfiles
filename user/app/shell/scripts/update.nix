43afc10f322a48c56eebfa6b182cef03689f738a
Mastermines1 <85805049+mastermines1@users.noreply.github.com>
2024-06-04 23:33:27 +0100

    175 current  2024-06-04 23:32:58  24.05.20240323.44d0940  6.6.22                          *

--- user/app/shell/scripts/update.nix
+++ user/app/shell/scripts/update.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = with pkgs; [
+    nh
     # Scripts
     (pkgs.writeScriptBin "update" (''
       #!/usr/bin/env bash
@@ -10,40 +11,39 @@
       pushd ''+settings.system.dotDir+'' > /dev/null
 
       case $1 in
+        "install")
+          echo "Not implemented yet...";;
+
+        "history")
+          git log -p $2;;
+
         "push")
           echo "Pushing to Origin..."
           git push origin main;;
 
+        "pull")
+          echo "Pulling from Origin..."
+          git pull;;
+
         "test")
-          git --no-pager diff -U0
-          echo "Building test config..."
           # Add all files to commit so the flake can see them.
           git add -A
-
-          # Build test system. Will use home manager if on another os, and nixos-rebuild if not.
-
-          nh os test -H system
-          echo "NixOS Test Built OK!";;
+          ${nh}/bin/nh os test . -H system $2;;
 
         "flake")
           echo "Updating flake lock file..."
           nix flake update .;;
 
         *)
-          git --no-pager diff -U0
-          echo "Building config and committing..."
           git add -A
           read -rp "Enter commit message (leave blank for generation number): " msg
-          #sudo nixos-rebuild switch --flake .#system -I ./hosts/${settings.system.profile}
-          sudo -E -u ${settings.user.username} nh os switch -H system
-
+          ${nh}/bin/nh os switch . -H system $1
 
           # If the user has entered no comit message, generate it.
           if ! [[ -n "$msg" ]]; then
             msg=$(nixos-rebuild list-generations | grep current)
           fi
           git commit -am "$msg"
-          echo "NixOS Rebuilt OK!";;
       esac
 
       popd > /dev/null
