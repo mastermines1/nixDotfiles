3b99496aa6e49433cc3c45b7591d89a282a63381
mastermines1 <milespatrickcampbell@gmail.com>
2024-06-09 15:59:50 +0100

    changed from p10k to oh-my-posh

--- user/app/shell/default.nix
+++ user/app/shell/default.nix
@@ -58,13 +58,6 @@ in
     ];
 
     initExtra = ''
-    # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-    # Initialization code that may require console input (password prompts, [y/n]
-    # confirmations, etc.) must go above this block; everything else may go below.
-    if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-      source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-    fi
-
     # Set the directory we want to store zinit and plugins
     ZINIT_HOME="''${XDG_DATA_HOME:-''${HOME}/.local/share}/zinit/zinit.git"
 
@@ -76,9 +69,6 @@ in
 
     # Source/Load zinit
     source "''${ZINIT_HOME}/zinit.zsh"
-
-    # Add in Powerlevel10k
-    zinit ice depth=1; zinit light romkatv/powerlevel10k
 
     # Add in snippets
     zinit snippet OMZP::git
@@ -94,8 +84,7 @@ in
 
     zinit cdreplay -q
 
-    # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-    [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    eval "$(oh-my-posh init zsh --config ''+settings.system.dotDir+''/user/app/shell/omp.toml)"
 
     zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 
@@ -137,6 +126,7 @@ in
     jq
     btop
     ffmpeg
+    oh-my-posh
   ];
 
   home.sessionVariables = {
